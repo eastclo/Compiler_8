f4e372dfb4f58a4c0f9ac22cc17dd00c0ca65c96
eastclo <goldeastcloud@gmail.com>
2020-12-12 18:13:15 +0900

    코드 생성기 완성

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "type.h"
 
+FILE* fout;
 FILE* yyin;
 int syntax_err;
 extern int semantic_err;
@@ -10,6 +12,12 @@ void main(int argc, char *argv[]) { //적당히 고쳐서 사용하세요
 	if ((yyin=fopen(argv[argc-1],"r"))==NULL){
 		printf("can not open input file: %s\n",argv[argc-1]);
 		exit(1);}
+	
+	if ((fout = fopen("a.asm", "w")) == NULL) {
+		printf("can't open output file: a.asm\n");
+		exit(1);
+	}
+
 	initialize();
 	yyparse();
 	if(!syntax_err)
@@ -17,5 +25,5 @@ void main(int argc, char *argv[]) { //적당히 고쳐서 사용하세요
 	semantic_analysis(root);
 	if(!semantic_err)
 		print_sem_ast(root);
-
+	code_generation(root);
 }
